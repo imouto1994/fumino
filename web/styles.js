a73fbe653668b6270dae99f3bf47a3e5a0db8838
Nhan Bui <imouto1994@gmail.com>
2019-12-30 20:51:51 +0700

    Update transition speed

--- web/styles.js
+++ web/styles.js
@@ -1,9 +1,10 @@
 module.exports = {
+  // Viewport
   mobileLandscape: "480px",
   tabletPortrait: "768px",
   tabletLandscape: "992px",
   dekstop: "1200px",
   // Transition
-  transitionColor: "color .6s ease",
-  transitionBackground: "background-color .6s ease",
+  transitionColor: "color .25s ease",
+  transitionBackground: "background-color .5s ease",
 };
