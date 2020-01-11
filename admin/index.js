71e9e8eb96aaace35da39f9084ed36a2b2092d53
Nhan Bui <imouto1994@gmail.com>
2020-01-12 05:56:28 +0800

    Update Admin panel

--- admin/index.js
+++ admin/index.js
@@ -1,22 +1,7 @@
 import CMS from "netlify-cms-app";
 
 import BookFetch from "./widgets/BookFetch";
-import BooksPreview from "./previews/BooksPreview";
 
 CMS.registerWidget("bookFetch", BookFetch);
-
-if (process.env.NODE_ENV === "production") {
-  const manifest = require("../build/manifest.json");
-  for (const fileName of Object.keys(manifest)) {
-    if (fileName.endsWith(".css")) {
-      CMS.registerPreviewStyle(manifest[fileName]);
-    }
-  }
-}
-
-CMS.registerPreviewTemplate("doujinshi", BooksPreview);
-CMS.registerPreviewTemplate("hentai", BooksPreview);
-CMS.registerPreviewTemplate("manga", BooksPreview);
-CMS.registerPreviewTemplate("digital", BooksPreview);
 
 CMS.init();
